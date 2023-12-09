61d7e5587c2e5060fda250b387fdaf1ac3760250
Kyomin Ku <moeday1010@gmail.com>
2023-12-09 20:45:28 +0900

    Endless level

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -5,14 +5,42 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] Transform[] levelParts;
-    [SerializeField] Transform respawnPosition;
+    [SerializeField] Vector3 nextPartPosition;
+
+    [SerializeField] float distanceToSpawn;
+    [SerializeField] float distanceToDelete;
+    [SerializeField] Transform player;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        GeneratePlatform();
+        DeletePlatform();
+    }
+
+    private void GeneratePlatform()
+    {
+        if (Vector2.Distance(player.position, nextPartPosition) < distanceToSpawn)
         {
             Transform randomPart = levelParts[Random.Range(0, levelParts.Length)];
-            Transform newPart = Instantiate(randomPart, respawnPosition.position, respawnPosition.rotation, transform).transform;
+
+            Vector2 newPosition = new Vector2(nextPartPosition.x - randomPart.Find("StartPoint").position.x, 0);
+            Transform newPart = Instantiate(randomPart, newPosition, transform.rotation, transform).transform;
+            nextPartPosition = newPart.Find("EndPoint").position;
+        }
+    }
+
+    private void DeletePlatform()
+    {
+        // 자식 게임 오브젝트에 대한 정보는 트랜스폼 컴포넌트가 갖는다
+        if (transform.childCount > 0)
+        {
+            Transform partToDelete = transform.GetChild(0);
+
+            if (Vector2.Distance(player.transform.position, partToDelete.position) > distanceToDelete)
+            {
+                // 가장 앞에 있는 플랫폼을 제거한다
+                Destroy(partToDelete.gameObject);
+            }
         }
     }
 }
