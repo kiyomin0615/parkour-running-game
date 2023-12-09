7d9488516d021232979d8a1f199fb9b513f24308
Kyomin Ku <moeday1010@gmail.com>
2023-12-09 19:33:07 +0900

    Generating platforms randomly

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] Transform[] levelParts;
+    [SerializeField] Transform respawnPosition;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Transform randomPart = levelParts[Random.Range(0, levelParts.Length)];
+            Transform newPart = Instantiate(randomPart, respawnPosition.position, respawnPosition.rotation, transform).transform;
+        }
+    }
+}
